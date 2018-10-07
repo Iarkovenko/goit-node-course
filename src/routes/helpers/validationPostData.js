476b058e14a7ed59f5aa2bc624e7a20e918cde77
Iarkovenko <yarkovenko@ukr.net>
2018-10-07 23:57:51 +0300

    finish Validate PostProductData

---
+++ src/routes/helpers/validationPostData.js
@@ -0,0 +1,13 @@
+const validateProductPostData = (incomingObj) => {
+    const keydefaultObj = ["name", "description", "price", "currency", "categories"];
+    const keyincomingObj = Object.keys(incomingObj);
+    let arr = [];
+    keydefaultObj.forEach(keydefault => {
+        if (!keyincomingObj.includes(keydefault)) {
+            arr.push(keydefault);
+        }
+    })
+    return arr;
+}
+
+module.exports = validateProductPostData;
--- src/routes/products/postNewProduct.js
+++ src/routes/products/postNewProduct.js
@@ -1,38 +1,56 @@
 const fs = require("fs");
 const path = require("path");
+const validateRequsetdata = require("../helpers/validationPostData")
 
 const postNewProduct = (request, response) => {
-  var newProduct = ''
-  request.on("data", function(data) {
-    newProduct = JSON.parse(data);
-    const obj = JSON.parse(data);
-    fs.writeFile(
-      __dirname + `../../../../products/${obj.id}.json`,
-      data,
-      err => {
-        if (err) throw err;
-        console.log("Saved!");
-      }
-    );
+  var newProduct = '';
+  let missingKeys = '';
+  request.on("data", function (data) {
+    const check = validateRequsetdata(JSON.parse(data));
+    
+    if (check.length !== 0) {
+      missingKeys = check;
+    } else {
+      const obj = JSON.parse(data);
+      newProduct = JSON.parse(data);
+      fs.writeFile(
+        __dirname + `../../../../products/${obj.id}.json`,
+        data,
+        err => {
+          if (err) throw err;
+          console.log("Saved!");
+        }
+      );
+    }
+
   });
 
-  request.on("end", function() {
-    response.writeHead(200, {
-      "Content-Type": "text/html"
-    });
-    response.write(
-      JSON.stringify({
-        status: "success",
-        product: {
-          name: newProduct.name,
-          description: newProduct.description,
-          price: newProduct.price,
-          currency: newProduct.currency,
-          categories: newProduct.categories
-        }
-      })
-    );
-    response.end();
+  request.on("end", function () {
+    if (missingKeys.length !== 0) {
+      response.writeHead(424, {
+        "Content-Type": "text/html"
+      });
+      response.write("ERROR Your are missing the next field" + ' ' + missingKeys.join(', '));
+      response.end();
+    } else {
+      response.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      response.write(
+        JSON.stringify({
+          status: "success",
+          product: {
+            name: newProduct.name,
+            description: newProduct.description,
+            price: newProduct.price,
+            currency: newProduct.currency,
+            categories: newProduct.categories
+          }
+        })
+      );
+      response.end();
+    }
+
   });
 };
 
